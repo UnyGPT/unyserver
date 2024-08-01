cf33d241aa45e9f64877055710f6ec49432ea784
gmpassos <gracilianomp@gmail.com>
2024-08-01 00:53:42 -0300

    setup-api_server.sh

--- api/setup-api_server.sh
+++ api/setup-api_server.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-chmod 755 api_server
-sudo setcap 'cap_net_bind_service=+ep' api_server
+CONFIG_SH="$HOME/.unyserver-config.sh"
+
+echo "CONFIG_SH: $CONFIG_SH"
+
+# Check if the configuration file exists
+if [ ! -f "$CONFIG_SH" ]; then
+  echo "** Configuration file $CONFIG_SH not found!"
+  exit 1
+fi
+
+echo "================================================================="
+
+set -x
+
+source "$CONFIG_SH"
+
+cd "$PROJECT_API_DIR";
+
+chmod 755 $$API_SERVER_EXE
+sudo setcap 'cap_net_bind_service=+ep' $$API_SERVER_EXE
